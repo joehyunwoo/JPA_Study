98b78da34b4e99f79c87178813833843b0c08d36
joe <joehyunwoo080@gmail.com>
2024-10-08 14:56:45 +0900

    2024.10.08 - 활용1 끝

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,6 +1,5 @@
 package jpabook.jpashop.service;
 
-import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,8 +18,8 @@ public class ItemService {
     @Transactional
     public void updateItem(Long itemId, String name, int price, int stockQuantity ) {
         Item findItem = itemRepository.findOne(itemId);
-        findItem.setPrice(price);
         findItem.setName(name);
+        findItem.setPrice(price);
         findItem.setStockQuantity(stockQuantity);
     }
 
